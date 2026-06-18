cb1842b7e290c811b84d7c92c11f523541e7a80c
Safa <safaauliya.works@gmail.com>
2026-06-18 10:00:59 +0700

    fix project structure and add swift files

---
+++ cookable-ios/cookable-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cookable-ios
+//
+//  Created by Safa Auliya on 18/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cookable-ios/cookable-ios/cookable_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookable_iosApp.swift
+//  cookable-ios
+//
+//  Created by Safa Auliya Hidayat on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct cookable_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
