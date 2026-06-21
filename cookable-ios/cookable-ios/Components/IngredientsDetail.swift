05bb46f11c377229a72f94ddf657826ab2b25259
Safa <safaauliya.works@gmail.com>
2026-06-21 21:55:52 +0700

    feat new data image for each ingredients

---
+++ cookable-ios/cookable-ios/Components/IngredientsDetail.swift
@@ -0,0 +1,8 @@
+//
+//  IngredientsDetail.swift
+//  cookable-ios
+//
+//  Created by Safa Auliya Hidayat on 21/06/26.
+//
+
+import Foundation
---
+++ cookable-ios/cookable-ios/View/DetailRecipeView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailRecipeView.swift
+//  cookable-ios
+//
+//  Created by Safa Auliya Hidayat on 21/06/26.
+//
+
+import Foundation
