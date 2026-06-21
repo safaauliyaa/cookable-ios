130520fd103b4aeef2cb6e3ff8c777fa96756071
Safa <safaauliya.works@gmail.com>
2026-06-21 12:54:50 +0700

    Fix bottom sheet title

--- cookable-ios/cookable-ios/Components/IngredientInfoSheet.swift
+++ cookable-ios/cookable-ios/Components/IngredientInfoSheet.swift
@@ -2,14 +2,38 @@
 //  IngredientInfoSheet.swift
 //  cookable-ios
 //
-//  Created by Safa Auliya Hidayat on 19/06/26.
+//  Created by Safa Auliya on 19/06/26.
 //
 
 import SwiftUI
 
 struct IngredientInfoSheet: View {
+
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
-        Text("Hello, World!")
+
+        NavigationStack {
+
+            VStack {
+                
+                
+                
+            }
+            .navigationTitle("Current Ingredients")
+            .navigationBarTitleDisplayMode(.inline)
+
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                    }
+                }
+            }
+        }
     }
 }
 
